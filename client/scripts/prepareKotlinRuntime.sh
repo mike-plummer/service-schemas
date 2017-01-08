1372e4ffbecca7babf5ad393e876d863416f329e
Mike Plummer <mike-plummer@users.noreply.github.com>
2017-01-08 14:52:14 -0600

    Comments

--- client/scripts/prepareKotlinRuntime.sh
+++ client/scripts/prepareKotlinRuntime.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Note for Windows users - I'm sorry, but there is no BATCH or POWERSHELL version
+# of this script so you'll have to do this manually or re-script.
+
+# Intent of this script is to download the 'kotlin-js-library JAR from a public repo,
+# expand, and extract the 'kotlin.js' runtime library for inclusion in our webpage.
+# Most of the logic in this script just tries to avoid re-downloading/reextracting if
+# the artifacts already exist.
+
 KOTLIN_VERSION=1.1-M04
 JAR_FILE=lib/kotlin-js.jar
 DESTINATION=lib/kotlin-js
