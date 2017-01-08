d7117ffb7da788b73a80f79c62107d46d6d667c1
Mike Plummer <mike-plummer@users.noreply.github.com>
2017-01-08 14:48:55 -0600

    Outputting JSON schema

--- client/src/schemas/json/json-schema.component.ts
+++ client/src/schemas/json/json-schema.component.ts
@@ -6,11 +6,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JsonSchemaComponent implements OnInit {
 
-    public resources: any[];
+    public resources: any[][];
 
     ngOnInit(): void {
-        const schema = require('../../../../java-schema/json-schema.json');
+        const schema: any = require('../../../../java-schema/json-schema.json');
 
-        console.log('Java schema loaded');
+        this.resources = Reflect.ownKeys(schema.definitions)
+            .map(resourceName => [resourceName, this.buildProperties(schema.definitions[resourceName])]);
+    }
+
+    private buildProperties(resource): any[] {
+        const properties: any[] =  Reflect.ownKeys(resource.properties)
+            .map(name => this.buildProperty(name, resource.properties[name]));
+
+        properties.forEach(property => property.required = resource.required && resource.required.includes(property.name));
+
+        return properties;
+    }
+
+    private buildProperty(name, prop): any {
+        const property = {
+            name,
+            type: prop.type,
+            required: false
+        };
+
+        if (property.type === 'array') {
+            const arrayType = prop.items.$ref;
+            property.type = `${property.type}[${arrayType.substring(arrayType.lastIndexOf('/')+1)}]`
+        } else if (prop.enum) {
+            property.type = `Enum [${prop.enum}]`;
+        } else if (prop.$ref) {
+            property.type = prop.$ref.substring(prop.$ref.lastIndexOf('/')+1);
+        }
+
+        if (prop.requiredProperties && prop.requiredProperties.includes(name)) {
+            property.required = true;
+        }
+
+        return property;
     }
 }
